b95541e4da765f8f183245dd6dab1a452a9ab3e6
josh rogerson <jrogerson@my.bcit.ca>
2022-11-15 09:54:37 -0800

    Multiple paramters in a function

--- script/script.js
+++ script/script.js
@@ -14,13 +14,22 @@
 // }
 
 
-let greet = (person_name) =>
-{
+// let greet = (person_name) =>
+// {
 
-    console.log("Hello " + person_name);
+//     console.log("Hello " + person_name);
 
+
+
+// }
+
+function greet (person_name,time = "day"){
+
+
+console.log("Good "+ time  + person_name);
 
 
 }
-greet("Neda");
+
+greet("Neda","Night");
 
