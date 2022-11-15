0c0cde8249635c2d2811876ec673d8ecf1e14da4
josh rogerson <jrogerson@my.bcit.ca>
2022-11-15 09:45:03 -0800

    Different types of function definitions

---
+++ script/script.js
@@ -0,0 +1,26 @@
+// function greet (person_name){
+
+
+// console.log("Hello " + person_name);
+
+
+// }
+
+// let greet = function (person_name)
+// {
+
+//     console.log("Hello " + person_name);
+
+// }
+
+
+let greet = (person_name) =>
+{
+
+    console.log("Hello " + person_name);
+
+
+
+}
+greet("Neda");
+
