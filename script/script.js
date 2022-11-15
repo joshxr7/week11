de8c6544e423e52349605dd41d58ee93bd170061
josh rogerson <jrogerson@my.bcit.ca>
2022-11-15 11:03:25 -0800

    list and image update

--- script/script.js
+++ script/script.js
@@ -42,15 +42,41 @@ for (let i = 0; i < list.length; i++ )
 {
 
     console.log(list[i]);
-    const listItemElement = document.createElement("li")
+    const listItemElement = document.createElement("li");
     listItemElement.textContent = list[i];
     unorderedListElement.appendChild(listItemElement);
 
 }
+}
+
+let shoppingList = ["cheese","bread","ham"];
+
+populateList(shoppingList);
+
+
+function listType(){
+
+    unorderedListElement.classList.add("squareList");
+    unorderedListElement.classList.remove("circleList");
+
+
+
+
+}
+listType();
+
+
+function updateImage(){
+
+
+const imageElement = document.querySelector("#shoppingCart")
+imageElement.src = "https://cdn-icons-png.flaticon.com/512/263/263142.png";
+imageElement.width = 100;
+imageElement.height = 100;
+imageElement.alt = "shopping cart"
+
 
 
 }
 
-let shoppingList = ["cheese","bread","ham"]
-
-populateList(shoppingList);
+updateImage();
